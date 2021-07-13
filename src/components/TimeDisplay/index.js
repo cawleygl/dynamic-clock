05d66e85e3f2c1737e104c8cf32af442eee4c9b0
cawleygl <grantlcawley@gmail.com>
2021-07-13 15:47:26 -0400

    Added clock that updates each second using state, useEffect, and setInterval

--- src/components/TimeDisplay/index.js
+++ src/components/TimeDisplay/index.js
@@ -1,9 +1,27 @@
+import React, { useState, useEffect } from "react";
+// import Moment from 'react-moment';
+import 'moment-timezone';
+
 import './style.css';
 
-function App() {
-  return (
-      <div>00:00</div>
-  );
+function TimeDisplay() {
+
+    const [time, setTime] = useState(null);
+
+    const currentTime = new Date().toLocaleString()
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(time => currentTime);
+        }, 1000);
+        return () => clearInterval(interval);
+      },
+      );
+
+    return (
+        <p className="Clock">Date: {time}</p>
+    );
 }
 
-export default App;
+
+export default TimeDisplay;
