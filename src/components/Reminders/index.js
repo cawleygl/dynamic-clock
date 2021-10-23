e0f67ab043e4e5005897ba7bb33fe2b20e8bf3da
Grant Cawley <grantlcawley@gmail.com>
2021-10-23 14:41:36 -0400

    Adjusted state variables

--- src/components/Reminders/index.js
+++ src/components/Reminders/index.js
@@ -1,55 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Container, Form, ListGroup } from 'react-bootstrap';
 
 import './style.css';
 
 function Reminders() {
-  const [reminder, setReminder] = useState({
-    current: null,
-    saved: JSON.parse(localStorage.getItem('ReminderList')) || []
-  });
+  const [currentReminder, setCurrentReminder] = useState(null);
+  const [savedReminder, setSavedReminder] = useState([]);
+
+  useEffect(() => {
+    if (savedReminder == []) {
+      setSavedReminder(JSON.parse(localStorage.getItem('ReminderList')));
+    };
+    
+    console.log(`Current: ${currentReminder}`, `Saved: `, savedReminder);
+  }, [currentReminder, savedReminder]);
+
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const retrieved = JSON.parse(localStorage.getItem('ReminderList'));
-    console.log("----");
-    console.log(retrieved);
-    console.log("----");
 
-    if (reminder.current === "" || reminder.current === null) {
-      console.log("BLANK");
+    if (currentReminder !== "" & currentReminder !== null) {
+      setSavedReminder(prevState => ([...prevState, { content: currentReminder.trim(), id: prevState.length }]));
 
-    } else if (retrieved === null) {
-      setReminder(prevState => ({
-        current: prevState.current,
-        saved: [{content: prevState.current, id: 0}]
-      }));
-      console.log("NOTHING IN LOCAL STORAGE");
-      localStorage.setItem('ReminderList', JSON.stringify([{content: reminder.current, id: 0}]));
-
-
-    } else {
-      setReminder(prevState => ({
-        current: prevState.current,
-        saved: [...prevState.saved, {content: prevState.current, id: prevState.saved.length}]
-      }));
       console.log("ADDING TO LOCAL STORAGE");
-      localStorage.setItem('ReminderList', JSON.stringify([...reminder.saved, {content: reminder.current, id: reminder.saved.length}]));
+      localStorage.setItem('ReminderList', JSON.stringify([...savedReminder, { content: currentReminder.trim(), id: savedReminder.length }]));
 
     }
-
-    console.log(reminder);
-
 
   }
 
   const onChange = (event) => {
 
-    setReminder(prevState => ({
-      current: event.target.value,
-      saved: prevState.saved
-    }));
-    console.log(reminder);
+    setCurrentReminder(event.target.value);
 
   }
 
@@ -58,7 +40,7 @@ function Reminders() {
     <Container fluid>
       <p className="Label">Reminders:</p>
       <ListGroup variant="flush">
-        {reminder.saved.map((reminderMap) => <ListGroup.Item key={reminderMap.id}>{reminderMap.content}</ListGroup.Item>)}
+        {savedReminder.map((reminderMap, index) => <ListGroup.Item key={reminderMap.id}>{reminderMap.content}: #{reminderMap.id}, Index: {index}</ListGroup.Item>)}
       </ListGroup>
       <Form.Control id="reminderInput" as="textarea" rows={1} onChange={onChange} />
       <Button id="reminderSubmit" variant="primary" size="lg" className="ReminderButton" onClick={handleSubmit}>Submit</Button>
